133fc7a6acc45d8316ca7d19667f1db8ca6ad50c
GerLanda <raysixsix9@gmail.com>
2021-03-06 12:19:58 +0300

    Ver1.4 теперь у меня 2 кота

--- src/model/Application.java
+++ src/model/Application.java
@@ -1,12 +1,19 @@
 package model;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Kotik garfild = new Kotik("Гарфилд", 10, 4, 5);
+        Kotik tom = new Kotik();
 
-        garfild.catInfo();
-        garfild.eat();
-        garfild.catInfo();
+        tom.setName("Том");
+        tom.setAge(5);
+        tom.setWeight(5);
+        tom.eat(5);
+
+        tom.lveAnotherDay();
+
+
+
 
     }
 }
--- src/model/Kotik.java
+++ src/model/Kotik.java
@@ -1,6 +1,9 @@
 package model;
 
 
+import java.util.Scanner;
+
+
 public class Kotik {
     private String name;
     private int weight;
@@ -14,11 +17,12 @@ public class Kotik {
         this.age = age;
         this.fuel = fuel;
         countCats++;
-        System.out.println("У нас появился новый котик, " + name + " теперь их: " + countCats);
+        System.out.println("У нас появился новый котик," + name + " Теперь их: " + countCats);
     }
 
     public Kotik() {
         countCats++;
+        System.out.println("Мы нашли нового кота на улице и ничего о нём не знаем.Теперь у нас их: "+countCats);
     }
 
     public String getName() {
@@ -58,7 +62,7 @@ public class Kotik {
             System.out.println("Котик, может съесть только от 1 до 5 порций!!!");
         }
         if (fuel > 0 && fuel < 6) {
-            this.fuel += fuel;
+            this.fuel += fuel+10;
             System.out.println("Отлично," + getName() + " покушал!!!");
         }
     }
@@ -69,17 +73,17 @@ public class Kotik {
 
             switch (food) {
                 case "Рыба":
-                    this.fuel += fuel + 3;
+                    this.fuel += fuel + 10;
                     System.out.println("Котик, любит рыбу!!!");
                     break;
 
                 case "Мясо":
-                    this.fuel += fuel + 1;
+                    this.fuel += fuel + 10;
                     System.out.println("Ммм.. вкусно , но лучше бы это была рыбка!!!");
                     break;
 
                 case "Сосиски":
-                    this.fuel += fuel;
+                    this.fuel += fuel+10;
                     System.out.println("Сосиски..?? ну ладно сойдет");
                     break;
 
@@ -107,10 +111,137 @@ public class Kotik {
                 food = "Сосиски";
                 break;
         }
-        eat(food,5);
+        eat(food, 5);
     }
 
+    public boolean play() {
+        if (fuel > 0) {
+            this.fuel = fuel - 5;
+            System.out.println("Котик поиграл...");
+            return true;
+        } else {
+            System.out.println("Котик голоден и не может играть!!!");
+            return false;
+        }
+    }
 
+    public boolean sleep() {
+        if (fuel > 0) {
+            System.out.println("Котик спит...");
+            this.fuel = fuel - 5;
+            return true;
+        } else {
+            System.out.println("Котик голоден и не может уснуть!!!");
+            return false;
+        }
+    }
+
+    public boolean chaseMouse() {
+        if (fuel > 0) {
+            System.out.println("Котик поймал мышь...");
+            this.fuel = fuel - 5;
+            return true;
+        } else {
+            System.out.println("Котик голоден и не может поймат мышь!!");
+            return false;
+        }
+    }
+
+    public boolean walkInTheStreet() {
+        if (fuel > 0) {
+            System.out.println("Котик  гуляет на улице!!!");
+            this.fuel = fuel - 5;
+            return true;
+        } else {
+            System.out.println("Котик голоден и не может пойти гулять!!!");
+            return false;
+        }
+    }
+
+    public boolean doNothing() {
+        if (fuel > 0) {
+            System.out.println("Котик бездельничает!!!");
+            this.fuel = fuel - 5;
+            return true;
+        } else {
+            System.out.println("Котик голоден!!!");
+            return false;
+        }
+    }
+
+    public void lveAnotherDay() throws InterruptedException {
+
+        for (int i = 0; i < 24; i++) {
+            if (fuel > 0) {
+                int j = (int) (Math.random() * 5) + 1;
+                switch (j) {
+                    case 1:
+                        play();
+                        Thread.sleep(3000);
+                        continue;
+
+                    case 2:
+                        sleep();
+                        Thread.sleep(3000);
+                        continue;
+
+
+                    case 3:
+                        chaseMouse();
+                        Thread.sleep(3000);
+                        continue;
+
+                    case 4:
+                        walkInTheStreet();
+                        Thread.sleep(3000);
+                        continue;
+                    case 5:
+                        doNothing();
+                        Thread.sleep(3000);
+                        continue;
+                }
+            } else {
+                System.out.println("Кота нужно покомрить введите \"Кушать\"");
+                Scanner console = new Scanner(System.in);
+                if (console.nextLine().equals("Кушать")) {
+                    int j = 0;
+                    j = (int) (Math.random() * 3) + 1;
+                    switch (j) {
+                        case 1:
+                            eat(5);
+                            continue;
+
+                        case 2:
+                            String randomFood = "";
+                            int foodGnerate = (int) (Math.random() * 3) + 1;
+                          if(foodGnerate==1){
+                              randomFood = "Рыба";
+                          }
+
+                          else if(foodGnerate==2){
+                              randomFood = "Мясо";
+                          }
+                          else if(foodGnerate==3){
+                              randomFood = "Сосиски";
+                          }
+                            eat(randomFood,5);
+                          continue;
+
+                        case 3:
+                            eat();
+                            continue;
+                    }
+                }
+                else{
+
+                    System.out.println("Такой команды я не понимаю ,дай \"Кушать\" ");
+
+                }
+            }
+
+        }
+        System.out.println("На сегодня всё, день закончен!");
+    }
 
 
 }
